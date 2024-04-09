20fe395b7435057fec80d3b3b7ad8c6c5d077516
gabrielvmelo <gvmgs@cin.ufpe.br>
2024-04-09 00:09:18 -0300

    set verbose line output to flush

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -463,7 +463,7 @@ def transcribe(
                 for segment in current_segments:
                     start, end, text = segment["start"], segment["end"], segment["text"]
                     line = f"[{format_timestamp(start)} --> {format_timestamp(end)}] {text}"
-                    print(make_safe(line))
+                    print(make_safe(line), flush=True)
 
             # if a segment is instantaneous or does not contain text, clear it
             for i, segment in enumerate(current_segments):
